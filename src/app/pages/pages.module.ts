2500837cacb8645fb46ac45a2a696da2a4f0b8c5
AlexisNarvaez <alexiszed19@gmail.com>
2019-08-28 11:08:23 -0500

    Rutas y modulas básico

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+
+@NgModule({
+  declarations: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  exports: [
+    AboutComponent,
+    ContactComponent,
+    HomeComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PagesModule { }
